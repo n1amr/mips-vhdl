567c567e37cb0613b552dbba42b02e6dbb0a0d87
MohamedA-M-Hassan <anehmr0@gmail.com>
2015-12-27 18:27:06 +0200

    control

--- control.vhd
+++ control.vhd
@@ -1,101 +1,109 @@
 library IEEE;
-use IEEE.std_logic_1164.ALL ;
-
+use IEEE.std_logic_1164.ALL ;	  
 entity control IS
-  port( opcode :in std_logic_vector (5 downto 0);
-        Branch, MemRead, MemWrite, ALUSrc, RegWrite , SignExtend: out std_logic;
-        ALUOp, RegDst, MemtoReg : out std_logic_vector(1 downto 0)
-      );
-end control;
+		  port (opcode :in std_logic_vector (5 downto 0) ;
+		  Branch, MemRead, MemWrite, ALUSrc, RegWrite,jump , signextend: out std_logic  ;
+		  ALUOp, RegDst, MemtoReg : out std_logic_vector(1 downto 0)  );
+	  end control ;
+	  architecture behave OF control IS						
+	  begin 
+      process (opcode)	
+	  begin 
+	-- 	 Rformat  
+  if (opcode = "000000"	)
+  then  RegDst <= "01" after 10 ns;
+        ALUSrc <= '0' after 10 ns;
+        MemtoReg <= "00"after 10 ns;
+        RegWrite <= '1'after 10 ns;
+        MemRead <= '0'after 10 ns;
+        MemWrite <= '0'after 10 ns;
+        Branch <= '0'after 10 ns;
+        ALUOp <= "10"after 10 ns;
+		jump <='0'after 10 ns;  
+		signextend <= '1' after 10 ns;
 
-architecture behave OF control IS
-begin
-  process (opcode)
-  begin
-    -- Rformat
-    if (opcode = "000000") then
-      RegDst <= "01" after 10ns ;
-      ALUSrc <= '0';
-      MemtoReg <= "00";
-      RegWrite <= '1';
-      MemRead <= '0';
-      MemWrite <= '0';
-      Branch <= '0';
-      ALUOp <= "10";
-      SignExtend <= '1';
-    end if;
-    -- addi
-    if (opcode = "001000") then
-      RegDst <= "00";
-      ALUSrc <= '1';
-      MemtoReg <= "00";
-      RegWrite <= '1';
-      MemRead <= '0';
-      MemWrite <= '0';
-      Branch <= '0';
-      ALUOp <= "00";
-      SignExtend <= '1';
-    end if;
-    -- andi
-    if (opcode = "001100") then
-      RegDst <= "00";
-      ALUSrc <= '1';
-      MemtoReg <= "00";
-      RegWrite <= '1';
-      MemRead <= '0';
-      MemWrite <= '0';
-      Branch <= '0';
-      ALUOp <= "11";
-      SignExtend <= '0';
-    end if;
+  end if  ; 
+	-- addi	 
+		 if (opcode = "001000"	)
+			  then	 RegDst <= "00"after 10 ns;
+        ALUSrc <= '1' after 10 ns;
+        MemtoReg <= "00" after 10 ns;
+        RegWrite <= '1'after 10 ns;
+        MemRead <= '0'after 10 ns;
+        MemWrite <= '0'after 10 ns;
+        Branch <= '0'after 10 ns;
+        ALUOp <= "00"after 10 ns;
+          jump <='0'after 10 ns;
+		  signextend <= '1' after 10 ns;
+		 end if  ;
+		  -- andi 
+  if (opcode = "001100"	)
+  then  RegDst <= "00"after 10 ns;
+        ALUSrc <= '1'after 10 ns;
+        MemtoReg <= "00"after 10 ns;
+        RegWrite <= '1'after 10 ns;
+        MemRead <= '0'after 10 ns;
+        MemWrite <= '0'after 10 ns;
+        Branch <= '0'after 10 ns;
+        ALUOp <= "11"after 10 ns;
+		jump <='0'after 10 ns;
+		signextend <= '0' after 10 ns;
 
-    -- lw
-    if (opcode = "100011") then
-      RegDst <= "00";
-      ALUSrc <= '1';
-      MemtoReg <= "01";
-      RegWrite <= '1';
-      MemRead <= '1';
-      MemWrite <= '0';
-      Branch <= '0';
-      ALUOp <= "00";
-      SignExtend <= '1';
-    end if;
-    -- sw
-    if (opcode = "101011") then
-      RegDst <= "XX";
-      ALUSrc <= '1';
-      MemtoReg <= "XX";
-      RegWrite <= '0';
-      MemRead <= '0';
-      MemWrite <= '1';
-      Branch <= '0';
-      ALUOp <= "00";
-      SignExtend <= '1';
-    end if;
-    -- beq
-    if (opcode = "000100") then
-      RegDst <= "XX";
-      ALUSrc <= '0';
-      MemtoReg <= "XX";
-      RegWrite <= '0';
-      MemRead <= '0';
-      MemWrite <= '0';
-      Branch <= '1';
-      ALUOp <= "01";
-      SignExtend <= '1';
-    end if;
-    -- jal
-    if (opcode = "000011") then
-      RegDst <= "10";
-      ALUSrc <= 'X';
-      MemtoReg <= "10";
-      RegWrite <= '1';
-      MemRead <= '0';
-      MemWrite <= '0';
-      Branch <= '0';
-      ALUOp <= "XX";
-      SignExtend <= '1';
-    end if;
-  end process;
-end behave;
+  end if  ;       
+  		   -- lw
+  if (opcode = "100011"	)
+  then  RegDst <= "00"after 10 ns;
+        ALUSrc <= '1'after 10 ns;
+        MemtoReg <= "01"after 10 ns;
+        RegWrite <= '1'after 10 ns;
+        MemRead <= '1'after 10 ns;
+        MemWrite <= '0'after 10 ns;
+        Branch <= '0'after 10 ns;
+        ALUOp <= "00"after 10 ns;
+        jump <='0'after 10 ns; 
+		signextend <= '1' after 10 ns;
+  end if  ;  
+  		-- sw
+  if (opcode = "101011"	)
+  then  RegDst <= "XX"after 10 ns;
+        ALUSrc <= '1'after 10 ns;
+        MemtoReg <= "XX"after 10 ns;
+        RegWrite <= '0'after 10 ns;
+        MemRead <= '0'after 10 ns;
+        MemWrite <= '1'after 10 ns;
+        Branch <= '0'after 10 ns;
+        ALUOp <= "00"after 10 ns;
+        jump <='0'after 10 ns;	
+		signextend <= '1' after 10 ns;
+
+  end if  ;	
+  		-- beq
+  if (opcode = "000100"	)
+  then  RegDst <= "XX";
+        ALUSrc <= '0' after 10 ns;
+        MemtoReg <= "XX" after 10 ns;
+        RegWrite <= '0'after 10 ns;
+        MemRead <= '0'after 10 ns;
+        MemWrite <= '0'after 10 ns;
+        Branch <= '1'after 10 ns;
+        ALUOp <= "01"after 10 ns;
+          jump <='0'after 10 ns;
+		 signextend <= '1' after 10 ns;
+
+  end if  ;
+  	 -- jal
+  if (opcode = "000011"	)
+  then  RegDst <= "10"after 10 ns;
+        ALUSrc <= 'X'after 10 ns;
+        MemtoReg <= "10"after 10 ns;
+        RegWrite <= '1'after 10 ns;
+        MemRead <= '0'after 10 ns;
+        MemWrite <= '0'after 10 ns;
+        Branch <= '0'after 10 ns;
+        ALUOp <= "XX"after 10 ns;
+		jump <='1'after 10 ns;
+		signextend <= '1' after 10 ns;
+  end if  ;
+      
+			  end process;
+		  end behave ;
